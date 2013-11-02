89371391fe1e4a91098ba44b474f57564c54241d
Denis Mereschenko <xproger.ru@gmail.com>
2013-11-02 16:09:22 +0200

    Small optimization for application menu builder

--- obmenu/Builder/Applications.py
+++ obmenu/Builder/Applications.py
@@ -11,18 +11,27 @@ from xdg import Config
 
 def make(root, paths):
     applicationElements = {}
+
+    for desktopEntryPath in __getDesktopFileList(paths):
+        desktopEntry = DesktopEntry.DesktopEntry(desktopEntryPath)
+        categories = [category for category in desktopEntry.getCategories()
+                      if category not in Ignore.list]
+        for category in categories:
+            if category not in applicationElements:
+                categoryName = category in Language.list and Language.list[category] or category
+                applicationElements[category] = SubElement(root, "menu",
+                                                           {"id": category,
+                                                            "icon": "",
+                                                            "label": categoryName})
+            iconPath = IconTheme.getIconPath(desktopEntry.getIcon(), 32, Config.icon_theme)
+            MenuItem.make(applicationElements[category], desktopEntry.getName(), desktopEntry.getExec(),
+                          iconPath)
+
+
+def __getDesktopFileList(paths):
     regexp = re.compile('.*?\.desktop$')
-    for dirName in paths:
-        for desktopEntryPath in listdir(dirName):
-            if re.match(regexp, desktopEntryPath):
-                desktopEntry = DesktopEntry.DesktopEntry(dirName + desktopEntryPath)
-                for category in desktopEntry.getCategories():
-                    if category in Ignore.list:
-                        continue
-                    if category not in applicationElements:
-                        categoryName = category in Language.list and Language.list[category] or category
-                        applicationElements[category] = SubElement(root, "menu",
-                                                                   {"id": category, "icon": "", "label": categoryName})
-                    iconPath = IconTheme.getIconPath(desktopEntry.getIcon(), 32, Config.icon_theme)
-                    MenuItem.make(applicationElements[category], desktopEntry.getName(), desktopEntry.getExec(),
-                                  iconPath)
+    desktopEntries = []
+    for desktopEntriesPath in paths:
+        desktopEntries += [desktopEntriesPath + desktopEntry for desktopEntry in listdir(desktopEntriesPath)
+                           if re.match(regexp, desktopEntry)]
+    return desktopEntries
